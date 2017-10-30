559c4b8221ed87b4c6e713a9c64a97bde608539a
moskomule <hataya@keio.jp>
2017-10-30 15:56:36 +0900

    fixed major bugs

--- exec.py
+++ exec.py
@@ -1,5 +1,4 @@
 from se_resnet import se_resnet50
-from se_inception import se_inception_v3
 from utils import Trainer
 
 import torch
@@ -9,9 +8,11 @@ from torchvision import datasets, transforms, models
 
 
 def main(batch_size=64):
-    transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                                    ])
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
     train_loader = torch.utils.data.DataLoader(
             datasets.CIFAR10('data/cifar10', train=True, download=True,
                              transform=transform),
@@ -21,9 +22,9 @@ def main(batch_size=64):
             batch_size=batch_size, shuffle=True)
     print("<<<<<< SERESNET >>>>>>")
     se_resnet = se_resnet50(num_classes=10)
-    optimizer = optim.Adam(params=se_resnet.parameters(), lr=1e-3)
+    optimizer = optim.Adam(params=se_resnet.parameters(), lr=1e-3, weight_decay=1e-4)
     trainer = Trainer(se_resnet, optimizer, F.cross_entropy)
-    trainer.loop(10, train_loader, test_loader)
+    trainer.loop(200, train_loader, test_loader)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ class Trainer(object):
     def _loop(self, data_loader, is_train=True):
         loop_loss = []
         correct = []
-        for idx, (data, target) in tqdm(enumerate(data_loader)):
+        for data, target in tqdm(data_loader):
             if self.cuda:
                 data, target = data.cuda(), target.cuda()
             data, target = Variable(data), Variable(target)
@@ -30,18 +30,16 @@ class Trainer(object):
             if is_train:
                 loss.backward()
                 self.optimizer.step()
-        print(f"loss: {sum(loop_loss):.2f}")
-        print(f"accuracy: {sum(correct):.2f}")
+        mode = "train" if is_train else "test"
+        print(f">>>[{mode}] loss: {sum(loop_loss):.2f}/accuracy: {sum(correct):.2f}")
         return loop_loss, correct
 
     def train(self, data_loader):
         self.model.train()
-        print(">>>train")
         loss, correct = self._loop(data_loader)
 
     def test(self, data_loader):
         self.model.eval()
-        print(">>>test")
         loss, correct = self._loop(data_loader, is_train=False)
 
     def loop(self, epochs, train_data, test_data):
